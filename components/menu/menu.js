6584666488b19f11a5fbc9f4e1e1c9281f054de1
mehulsatardekar <satardekar042197@gmail.com>
2022-02-09 19:17:57 +0530

    menu section for CL

---
+++ components/menu/menu.js
@@ -0,0 +1,14 @@
+const menuIcon = document.querySelector('#menu');
+const closeIcon = document.querySelector('#close')
+const sidebarMenu = document.querySelector('#sidebar-menu');
+
+
+menuIcon.addEventListener('click',()=>{
+    sidebarMenu.classList.remove('hide')
+    sidebarMenu.classList.toggle('sidebar-menu-width');
+})
+
+closeIcon.addEventListener('click',()=>{
+    sidebarMenu.classList.toggle('hide')
+    sidebarMenu.classList.remove('sidebar-menu-width')
+})
